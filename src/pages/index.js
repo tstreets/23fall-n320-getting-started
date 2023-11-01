b92ff8872ba4e5e9f5c723d85067da6aff246426
Ty <ty.streets@irvmat.com>
2023-11-01 19:38:33 -0400

    Update with good code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '@/styles/Home.module.css';
-import Hero from '@/components/hero'
+import Hero from '@/components/Hero'
 
 export default function Home() {
   return (
