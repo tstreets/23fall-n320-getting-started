060649c7369d5892be6253ac5548225aaf91f38f
Ty <ty.streets@irvmat.com>
2023-11-01 19:37:12 -0400

    Put bad code up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import styles from '@/styles/Home.module.css';
-import Hero from '@/components/Hero'
+import Hero from '@/components/hero'
 
 export default function Home() {
   return (
     <>
-      <Hero heroImage='/hero.jpg' height='200px' header='Main Hero' />
+      <Hero heroImage='/hero-image.jpg' height='200px' header='Main Hero' />
       <h1 className={styles.bigRedFont}>Home</h1>
       <p>This is the landing page for our Next.js app.</p>
       <p className={styles.blueFont}>This should be blue font.</p>
-      <Hero heroImage='/hero.jpg' height='600px' />
+      <Hero heroImage='/hero-image.jpg' height='600px' />
     </>
   );
 }
